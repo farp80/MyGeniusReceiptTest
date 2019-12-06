b42987d8159241a28ee9d779672f7d9df66a59b9
Fidel Rosell <farp80@gmail.com>
2019-12-05 19:37:12 -0500

    added model

--- api/models.py
+++ api/models.py
@@ -1,26 +1,78 @@
-"""
-All your application modules and serializers are going to be declared inside this file
-"""
-from rest_framework import serializers
 from django.db import models
-
-"""
-Define he Contact Entity into your applcation model
-"""
-class Contact(models.Model):
-    first_name = models.CharField(max_length=50, default='')
-    last_name = models.CharField(max_length=50, default='')
-    phone_number = models.CharField(max_length=15, default='')
-    email = models.CharField(max_length=150, default='')
-
-"""
-The ContactSerializer is where you will specify what properties
-from the ever Contact should be inscuded in the JSON response
-"""
-class ContactSerializer(serializers.ModelSerializer):
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from rest_framework import serializers
 
 
-    class Meta:
-        model = Contact
-        # what fields to include?
-        fields = ('first_name','last_name', 'phone_number', 'email')
+class MyAccountManager( BaseUserManager ):
+    def create_user(self, email, username, first_name, password=None):
+        if not email:
+            raise ValueError( 'Enter an email' )
+        if not username:
+            raise ValueError( 'Enter an username' )
+
+        user = self.model(
+            email=self.normalize_email( email ),
+            username=username,
+            first_name=first_name
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, first_name, email, password):
+        user = self.create_user(
+            email=self.normalize_email(email),
+            password=password,
+            username=username,
+            first_name=first_name)
+
+        user.is_admin=True
+        user.is_staff=True
+        user.is_superuser=True
+
+        user.save(using=self._db)
+        return user
+
+
+# Create your models here.
+class Users( AbstractBaseUser ):
+    username = models.CharField( max_length=255, unique=True )
+    email = models.EmailField( max_length=30, unique=True, verbose_name='email' )
+    date_joined = models.DateTimeField( verbose_name='date joined', auto_now_add=True )
+    last_login = models.DateTimeField( verbose_name='last login', auto_now_add=True )
+    is_active = models.BooleanField( default=True )
+    is_admin = models.BooleanField( default=False )
+    is_staff = models.BooleanField( default=False )
+    is_superuser = models.BooleanField( default=False )
+    first_name = models.CharField( max_length=50, verbose_name='first name' )
+    last_name = models.CharField( max_length=50, verbose_name='last name' )
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', 'first_name']
+
+    objects = MyAccountManager()
+
+    def __str__(self):
+        return self.email
+
+    def has_perm(self, perm, obj=None):
+        return self.is_admin
+
+    def has_module_perms(self, app_label):
+        return True
+
+class Recipes( models.Model ):
+    name = models.CharField( max_length=255, null=False )
+    user = models.OneToOneField( Users, on_delete=models.CASCADE )
+    # ingredients_set ([] --> list) --> all the ingredients in the recipes
+    # steps_set ([] --> list)
+
+class Steps( models.Model ):
+    step_text = models.TextField(max_length=5012, null=False )
+    recipe = models.ForeignKey( Recipes, on_delete=models.CASCADE, related_name='stepsForRecipe' )
+
+
+class Ingredients( models.Model ):
+    text = models.TextField( null=False )
+    recipe = models.ForeignKey(Recipes, on_delete=models.CASCADE, related_name='ingredientsForRecipe')
