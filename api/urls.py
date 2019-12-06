23eb50bc814b33821533e2a0bdcae8fcdd9375d5
farp80 <farp80@gmail.com>
2019-12-06 04:39:33 +0000

    missed PUT

--- api/urls.py
+++ api/urls.py
@@ -2,5 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from api import views
 urlpatterns = [
-    
+    path('recipes/', views.RecipesView.as_view(), name='recipes'),
+    path('recipes/<int:recipe_id>', views.RecipesView.as_view(), name='recipe-id'),
+    path('user/<int:user_id>', views.RecipesView.as_view(), name='user-id')
 ]
--- api/views.py
+++ api/views.py
@@ -1,14 +1,23 @@
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from api.models import Recipes
+from api.models import Recipes, Users
 
-class RecipesView(APIView):
-    def get(self, request, user_id=None):
+class UsersView(APIView):
+    def get(self,request,user_id=None):
         if user_id is not None:
             user = Users.objects.get(id = user_id)
             recipes = user.user_recipe.all()
             serializer = RecipesSerializer(recipes, many=True)
+            return Response(serializer.data)
+        else:
+           return Response(status=status.HTTP_200_OK)
+
+class RecipesView(APIView):
+    def get(self,request,recipe_id=None):
+        if recipe_id is not None:
+            recipe = Recipes.objects.get(id = user_id)
+            serializer = RecipesSerializer(recipe, many=False)
             return Response(serializer.data)
         else:
             allRecipes = Recipes.objects.all()
@@ -24,7 +33,15 @@ class RecipesView(APIView):
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-     def delete(self, request, recipe_id):
+    def delete(self,request,recipe_id):
         recipe = Recipes.objects.get(id=recipe_id)
         recipe.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+    #def put(self,request,recipe_id):
+        # I am not quite sure on how to do PUT (Update)
+        # I'd get the specific recipe_id and update its fields
+        # then save it in the db.
+        # of course, I would do some safe coding to validate the request data
+
+
